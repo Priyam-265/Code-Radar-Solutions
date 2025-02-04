ee9c2560996f071d86f56b05e40cc7f2fbe13502
Priyam-265 <priyamkathuria265@gmail.com>
2025-02-04 17:30:26 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,36 +3,36 @@ int main(){
     int a,b;
     char choice;
     scanf("%d %d %c",&a,&b,&choice);
-    // if(c=='+){
-    //     printf("%d",a+b);
-    // }
-    // else if(c=='-'){
-    //     printf("%d",a-b);
-    // }
-    // else if(c=='*){
-    //     printf("%d",a*b);
-    // }
-    // else if(c=='/'){
-    //     printf("%d",a/b);
-    // }
-    // else{
-    //     printf("error");
-    // }
-    switch(choice){
-    case '+':
-    printf("%d",a+b);
-    break;
-    case '-':
-    printf("%d",a-b);
-    break;
-    case '*':
-    printf("%d",a*b);
-    break;
-    case '/':
-    printf("%d",a/b);
-    break;
-    default:
-    printf("error");
+    if(choice=='+'){
+        printf("%d",a+b);
     }
+    else if(choice=='-'){
+        printf("%d",a-b);
+    }
+    else if(choice=='*){
+        printf("%d",a*b);
+    }
+    else if(choice=='/'){
+        printf("%d",a/b);
+    }
+    else{
+        printf("error");
+    }
+    // switch(choice){
+    // case '+':
+    // printf("%d",a+b);
+    // break;
+    // case '-':
+    // printf("%d",a-b);
+    // break;
+    // case '*':
+    // printf("%d",a*b);
+    // break;
+    // case '/':
+    // printf("%d",a/b);
+    // break;
+    // default:
+    // printf("error");
+    // }
     return 0;
     }
