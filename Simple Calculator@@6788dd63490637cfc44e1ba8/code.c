04c822035224d44ba2f6b9745a8df360ede1b243
Priyam-265 <priyamkathuria265@gmail.com>
2025-02-11 01:25:09 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,13 +7,13 @@ int main(){
     if(c='+'){
         printf("%d",a+b);
     }
-    else if(c=='-'){
+    else if(c='-'){
         printf("%d",a-b);
     }
-    else if(c=='*'){
+    else if(c='*'){
         printf("%d",a*b);
     }
-    else if(c=='/'){
+    else if(c='/'){
         printf("%d",a/b);
     }
     else{
