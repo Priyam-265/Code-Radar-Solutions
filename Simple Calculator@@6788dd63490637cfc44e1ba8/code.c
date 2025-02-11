dbaed6cdc22d8239ff1c68395c2d7f379aac8dbb
Priyam-265 <priyamkathuria265@gmail.com>
2025-02-11 16:50:50 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,28 +3,49 @@ int main(){
     int a,b;
     char c;
     scanf("%d %d%c",&a,&b,&c);
-    if(c=='+'){
-        int sum=a+b;
-        printf("%d",sum);
-    }
-    else if(c=='-'){
-        int diff=a-b;
-        printf("%d",diff);
-    }
-    else if(c=='*'){
-        int mul=a*b;
-        printf("%d",mul);
-    }
-    else if(c=='/'){
+    // if(c=='+'){
+    //     int sum=a+b;
+    //     printf("%d",sum);
+    // }
+    // else if(c=='-'){
+    //     int diff=a-b;
+    //     printf("%d",diff);
+    // }
+    // else if(c=='*'){
+    //     int mul=a*b;
+    //     printf("%d",mul);
+    // }
+    // else if(c=='/'){
+    //     if(b!=0){
+    //     int div=a/b;
+    //     printf("%d",div);
+    //     }
+    //     else{
+    //         printf("error");
+    //     }
+    // }
+    // else{
+    //     printf("error");
+    // }
+    switch(c){
+        case '+':
+        printf("%d",a+b);
+        break;
+        case'-':
+        printf("%d",a-b);
+        break;
+        case'*':
+        printf("%d",a*b);
+        break;
+        case'/':
         if(b!=0){
-        int div=a/b;
-        printf("%d",div);
+            printf("%d",a/b);
         }
         else{
             printf("error");
         }
-    }
-    else{
+        break;
+        default:
         printf("error");
     }
     return 0;
