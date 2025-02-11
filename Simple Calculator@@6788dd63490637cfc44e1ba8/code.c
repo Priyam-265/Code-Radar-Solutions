4b0056006fd0ecd2dbe664e866566d5139ef56b6
Priyam-265 <priyamkathuria265@gmail.com>
2025-02-11 17:06:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,50 +3,50 @@ int main(){
     float a,b;
     char c;
     scanf("%f %f %c",&a,&b,&c);
-    // if(c=='+'){
-    //     int sum=a+b;
-    //     printf("%d",sum);
-    // }
-    // else if(c=='-'){
-    //     int diff=a-b;
-    //     printf("%d",diff);
-    // }
-    // else if(c=='*'){
-    //     int mul=a*b;
-    //     printf("%d",mul);
-    // }
-    // else if(c=='/'){
-    //     if(b!=0){
-    //     int div=a/b;
-    //     printf("%d",div);
-    //     }
-    //     else{
-    //         printf("error");
-    //     }
-    // }
-    // else{
-    //     printf("error");
-    // }
-    switch(c){
-        case '+':
-        printf("%.0f",a+b);
-        break;
-        case'-':
-        printf("%.0f",a-b);
-        break;
-        case'*':
-        printf("%.0f",a*b);
-        break;
-        case'/':
+    if(c=='+'){
+        int sum=a+b;
+        printf("%.0f",sum);
+    }
+    else if(c=='-'){
+        int diff=a-b;
+        printf("%.0f",diff);
+    }
+    else if(c=='*'){
+        int mul=a*b;
+        printf("%.0f",mul);
+    }
+    else if(c=='/'){
         if(b!=0){
-            printf("%.0f",a/b);
+        int div=a/b;
+        printf("%.0f",div);
         }
         else{
             printf("error");
         }
-        break;
-        default:
-        printf("er");
     }
-    return 0;
+    else{
+        printf("error");
     }
+    // switch(c){
+    //     case '+':
+    //     printf("%.0f",a+b);
+    //     break;
+    //     case'-':
+    //     printf("%.0f",a-b);
+    //     break;
+    //     case'*':
+    //     printf("%.0f",a*b);
+    //     break;
+    //     case'/':
+    //     if(b!=0){
+    //         printf("%.0f",a/b);
+    //     }
+    //     else{
+    //         printf("error");
+    //     }
+    //     break;
+    //     default:
+    //     printf("er");
+    // }
+    // return 0;
+    // }
